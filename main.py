23ccea709bb7eae93223b64e8d2aaa5874f90112
Priyanshu Joshi <joshipriyanshu575@gmail.com>
2026-03-09 23:08:59 +0530

    last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, WebSocket
 import tempfile
 import os
+import wave
+import asyncio
 
 from stt import speech_to_text
 from ai import ask_ai
@@ -26,41 +28,59 @@ async def voice_assistant(websocket: WebSocket):
 
         while True:
 
-            chunk = await websocket.receive_bytes()
+            message = await websocket.receive()
 
-            if chunk == b"STOP":
-                break
+            if "bytes" in message:
+                audio_data += message["bytes"]
 
-            audio_data += chunk
+            elif "text" in message:
+                if message["text"] == "STOP":
+                    break
+
+        print("Audio size:", len(audio_data))
 
         with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f:
-            f.write(audio_data)
             audio_path = f.name
 
+        with wave.open(audio_path, "wb") as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(16000)
+            wf.writeframes(audio_data)
+
         text = speech_to_text(audio_path)
+
         print("User:", text)
 
         os.remove(audio_path)
 
         response = ask_ai(text)
+
         print("AI:", response)
 
-    
         audio_file = text_to_speech(response)
 
         with open(audio_file, "rb") as f:
+
             while True:
-                chunk = f.read(1024)
+
+                chunk = f.read(4096)
+
                 if not chunk:
                     break
+
                 await websocket.send_bytes(chunk)
+
+                await asyncio.sleep(0.01)
 
         await websocket.send_bytes(b"END")
 
         os.remove(audio_file)
 
     except Exception as e:
-        print("Error:", e)
+
+        print("Server error:", e)
 
     finally:
+
         await websocket.close()
